7c09b8f90de7401c12a8e6f8683a6c1ad8c5e480
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-08-08 20:37:14 -0300

    Filtro funcional

--- src/components/TasksSection.jsx
+++ src/components/TasksSection.jsx
@@ -4,16 +4,16 @@ import styles from './TasksSection.module.css'
 export const TasksSection = ({tasks,deleteTask,setTaskStatus,filter}) => {
     //Função para estilizar exibição do nome da tarefa
     function stylizeTaskName(index) {
-        if (tasks[index].status == 'Concluída') {
+        if (filteredTasks[index].status == 'Concluída') {
             return styles.taskCompleted
         }
     }
 
     //Estilizar o botão de definir status da tarefa
     function stylizeCompletionButton(index) {
-        if (tasks[index].status == 'Pendente') {
+        if (filteredTasks[index].status == 'Pendente') {
             return styles.conclude
-        } else if (tasks[index].status == 'Concluída') {
+        } else if (filteredTasks[index].status == 'Concluída') {
             return styles.pendant
         }
     }
@@ -26,10 +26,22 @@ export const TasksSection = ({tasks,deleteTask,setTaskStatus,filter}) => {
             return 'Marcar como pendente'
         }
     }
+
+    function getFilteredTaskArray() {
+        if (filter == '') {
+            return tasks
+        } else {
+            return (
+            tasks.filter((element) => element.status == filter)
+            )
+        }
+    }
+
+    let filteredTasks = getFilteredTaskArray()
   
     return (
     <div className={styles.TasksSectionContent}>
-        {tasks.map((element,index) => {
+        {filteredTasks.map((element,index) => {
             return (                
                 <div className={styles.divTask}> 
                     <span className={stylizeTaskName(index)}>
